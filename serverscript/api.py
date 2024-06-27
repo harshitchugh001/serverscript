0cd3e4659fbe43502f5e4c4874a8921ac88a3342
harshitchugh001 <harshitchugh001@gmail.com>
2024-06-27 16:06:36 +0530

    api

---
+++ serverscript/api.py
@@ -0,0 +1,106 @@
+
+
+import frappe
+from frappe import _
+
+
+@frappe.whitelist()
+def get_items():
+    if(frappe.local.request.method != "GET"):
+        # frappe.throw("only post request si accepted")
+        return "only GET request is accepted"
+    doctype = frappe.local.form_dict.get('doctype')
+    name = frappe.local.form_dict.get("name")
+    if not doctype:
+        return {"error": "Required :- doctype "}, 400
+
+    if(name):
+        try:
+            item = frappe.get_doc(doctype, name)
+            return item
+        except Exception as e:
+            return {"error": str(e)}, 500
+        
+    all_items=[]
+   
+    try:
+        records = frappe.get_all(doctype, fields=["*"])
+        for record in records:
+            all_items.append(frappe.get_doc(doctype,record))
+        return all_items
+    except Exception as e:
+        return {"error": str(e)}, 500
+@frappe.whitelist()
+def create_item():
+    if(frappe.local.request.method != "POST"):
+        # frappe.throw("only post request si accepted")
+        return "only POST request is accepted"
+    data = frappe.local.form_dict
+    doctype = data.get('doctype')
+    if not doctype:
+        return {"error": "Required :- doctype"}, 400
+
+    try:
+        doc = frappe.get_doc({
+            "doctype": doctype,
+            **data
+        })
+        doc.insert()
+        return doc.as_dict()
+    except Exception as e:
+        return {"error": str(e)}, 500
+@frappe.whitelist()
+def update_item():
+    if(frappe.local.request.method != "PUT"):
+        # frappe.throw("only post request si accepted")
+        return "only PUT request is accepted"
+    data = frappe.local.form_dict
+    doctype = data.get('doctype')
+    name = data.get('name')
+    if not doctype or not name:
+        return {"error": "Required :- doctype and name "}, 400
+
+    try:
+        doc = frappe.get_doc(doctype, name)
+        doc.update(data)
+        doc.save()
+        return doc.as_dict()
+    except Exception as e:
+        return {"error": str(e)}, 500
+@frappe.whitelist()
+def delete_item():
+    if(frappe.local.request.method != "DELETE"):
+        # frappe.throw("only post request si accepted")
+        return "only DELETE request is accepted"
+    data = frappe.local.form_dict
+    doctype = data.get('doctype')
+    name = data.get('name')
+    if not doctype or not name:
+        return {"error": "!Required:- doctype and name"}, 400
+
+    try:
+        frappe.delete_doc(doctype, name)
+        return {"message": f"Item {name} deleted successfully"}
+    except Exception as e:
+        return {"error": str(e)}, 500
+    
+
+
+@ frappe.whitelist()
+def get_users():
+    return frappe.get_all("User",fields=["*"])
+
+
+@ frappe.whitelist()
+def add_user():
+    if(frappe.local.request.method != "POST"):
+        # frappe.throw("only post request si accepted")
+        return "only POST request is accepted"
+    data = frappe.local.form_dict
+    try:
+        user = frappe.get_doc({"doctype":"User",**data})
+        user.insert()
+        frappe.db.commit()
+        return user.as_dict()
+    except Exception as e:
+        return {"error": str(e)},500
